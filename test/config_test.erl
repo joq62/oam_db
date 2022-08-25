91b43b79af232a56443210ccaa602a8fe0078a54
joq62 <joakim.leche@gmail.com>
2022-08-25 23:35:01 +0200

    ud

--- test/config_test.erl
+++ test/config_test.erl
@@ -18,8 +18,9 @@
 %-define(HostNames,["c200","c201"]).
 %-define(HostNames,["c100"]).
 
--define(HostNames,["c100","c200","c201"]).
-
+-define(AllHostNames,["c100","c200"]).
+-define(ApplInfoSpecs,{"application_info_specs",".spec"}).
+-define(HostInfoSpecs,{"host_info_specs",".host"}).
 %% --------------------------------------------------------------------
 %% Function: available_hosts()
 %% Description: Based on hosts.config file checks which hosts are avaible
@@ -28,17 +29,48 @@
 start()->
     ok=setup(),
      
-    R1=[{HostName,oam_db:create_config_node(HostName)}||HostName<-?HostNames],
-    R11=[{HostName,Node}||{HostName,{ok,Node}}<-R1],
-  %  {ok,'config@c201'}=oam_db:create_config_node(H1),
- 
-    R2=[rpc:call(Node,config,host_local_ip,[HostName])||{HostName,Node}<-R11],
-    io:format("~p~n",[{lists:sort(R2),?MODULE,?FUNCTION_NAME,?LINE}]),
+    {ok,AllConfigNodes}=create_config_node_test(),
+    io:format("AllConfigNodes ~p~n",[{AllConfigNodes,?MODULE,?FUNCTION_NAME,?LINE}]),
+    
+    ok=copy_dir_test(?ApplInfoSpecs,AllConfigNodes),
+    
  
     
-    ok=cleanup(),
+ %   ok=cleanup(),
     io:format("TEST Ok, there you go! ~p~n",[{?MODULE,?FUNCTION_NAME,?LINE}]),
     ok.
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+copy_dir_test({SourceDir,Ext},AllConfigNodes)->
+    DeleteDir1=[rm:r(N,SourceDir)||N<-AllConfigNodes],
+    io:format("DeleteDir1 ~p~n",[{DeleteDir1,?MODULE,?FUNCTION_NAME,?LINE}]),
+    
+    MkDir=[{N,rpc:call(N,file,make_dir,[SourceDir],5000)}||N<-AllConfigNodes],
+    io:format("MkDir ~p~n",[{MkDir,?MODULE,?FUNCTION_NAME,?LINE}]),
+
+    SourceNode=node(),
+    DestDir=SourceDir,
+    CopyResult=[copy:copy_dir_ext(SourceNode,SourceDir,DestNode,DestDir,Ext)||DestNode<-AllConfigNodes],
+    io:format("CopyResult ~p~n",[{CopyResult,?MODULE,?FUNCTION_NAME,?LINE}]),
+
+    DeleteDir2=[rm:r(N,SourceDir)||N<-AllConfigNodes],
+    io:format("DeleteDir2 ~p~n",[{DeleteDir2,?MODULE,?FUNCTION_NAME,?LINE}]),
+    ok.
+
+%% --------------------------------------------------------------------
+%% Function: available_hosts()
+%% Description: Based on hosts.config file checks which hosts are avaible
+%% Returns: List({HostId,Ip,SshPort,Uid,Pwd}
+%% --------------------------------------------------------------------
+create_config_node_test()->
+    R1=[{HostName,oam_db:create_config_node(HostName)}||HostName<-?AllHostNames],
+    AllConfigNodes=[Node||{_HostName,{ok,Node}}<-R1],
+    {ok,AllConfigNodes}.
+
 
 %% --------------------------------------------------------------------
 %% Function: available_hosts()
@@ -48,9 +80,9 @@ start()->
 cleanup()->
     OamTestLogsDir="test_logs",
     []=os:cmd("rm -rf "++OamTestLogsDir),
-    Nodes=[list_to_atom("config@"++HostName)||HostName<-?HostNames],
+    Nodes=[list_to_atom("config@"++HostName)||HostName<-?AllHostNames],
     [rpc:call(Node,init,stop,[])||Node<-Nodes],
-    [ssh_vm:delete(HostName,"config")||HostName<-?HostNames],
+    [ssh_vm:delete(HostName,"config")||HostName<-?AllHostNames],
     ok.
     
 %% --------------------------------------------------------------------
@@ -79,6 +111,8 @@ setup()->
 %    appl:stop('config@c100',config),
  %   appl:unload('config@c100',config,"config"),
 
-    [rpc:call(N,init,stop,[])||N<-['config@c100','config@c201']],
+    Nodes=[list_to_atom("config@"++HostName1)||HostName1<-?AllHostNames],
+    [erlang:set_cookie(N,config)||N<-Nodes],
+    [rpc:call(N,init,stop,[])||N<-Nodes],
     io:format("SUB-TEST OK  ~p~n",[{?MODULE,?FUNCTION_NAME,?LINE}]), 
     ok.
